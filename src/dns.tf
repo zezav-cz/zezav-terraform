1838f3b01d06882f9b98594f97cd972a5f712372
Jan Trojak <trojakjan24@gmail.com>
2026-05-13 20:09:21 +0200

    Base

--- src/dns.tf
+++ src/dns.tf
@@ -5,9 +5,8 @@ resource "hcloud_zone" "zezav_cz" {
   delete_protection = true
 }
 
-
 resource "hcloud_zone_rrset" "zezav_cz_a_records" {
-  for_each = toset(["@", "www", "dir", "blog"])
+  for_each = toset(["@", "www", "dir", "blog", "private"])
   zone     = hcloud_zone.zezav_cz.name
   name     = each.key
   type     = "A"
@@ -17,6 +16,17 @@ resource "hcloud_zone_rrset" "zezav_cz_a_records" {
   ]
 }
 
+resource "hcloud_zone_rrset" "vpn_zezav_cz" {
+  zone = hcloud_zone.zezav_cz.name
+  name = "vpn"
+  type = "A"
+
+  records = [
+    { value = hcloud_server.z03_de.ipv4_address }
+  ]
+}
+
+# == Github ==
 resource "hcloud_zone_rrset" "zezav_cz_gh_txt" {
   zone = hcloud_zone.zezav_cz.name
   name = "_github-pages-challenge-zezav-cz"
@@ -27,3 +37,39 @@ resource "hcloud_zone_rrset" "zezav_cz_gh_txt" {
   ]
 }
 
+# == Lara ==
+resource "hcloud_zone_rrset" "zezav_cz_lara_mx" {
+  zone = hcloud_zone.zezav_cz.name
+  name = "@"
+  type = "MX"
+  ttl  = 600
+
+  records = [
+    { value = "1 mx1.larksuite.com." },
+    { value = "5 mx2.larksuite.com." },
+    { value = "10 mx3.larksuite.com." }
+  ]
+}
+
+resource "hcloud_zone_rrset" "zezav_cz_lara_txt" {
+  zone = hcloud_zone.zezav_cz.name
+  name = "@"
+  type = "TXT"
+  ttl  = 600
+
+  records = [
+    { value = provider::hcloud::txt_record("verification-code-site-App_lark=nrS1tj9eN8cJtHPx3cSo") },
+    { value = provider::hcloud::txt_record("v=spf1 +include:spf.onlarksuite.com -all") }
+  ]
+}
+
+resource "hcloud_zone_rrset" "zezav_cz_lara_txt_dkim" {
+  zone = hcloud_zone.zezav_cz.name
+  name = "lark2603032321._domainkey"
+  type = "TXT"
+  ttl  = 600
+
+  records = [
+    { value = provider::hcloud::txt_record("v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3X1h+GexBGEThr5HIpL5Q/6Lv6Xk54Wo5k35EQSQ0Bauxg7uvJ40JuTn0Zqwu0ixzeeV0w3IO0zJMGOgHC2ouZ14EGyu/uEWhyQQVvJh6JKMFOo/OAya+b3MIKHMneGm7Pw2RjqEIRnT3Grw8XYp+ksUoJrznn+DKVg5EOP8XwELhacQF6twMGmSqjUEideN9RAw0QmVkbnf6p0Lp4+f1tTu0hA9NPwwe+0jAY0FEBr4McnzAvHPwswFyuhAjAQn8wFYUWz3jxxT8xe3bdBPzYkm+v6K9p1X2LWYJv0rXm2Sp8/YoH97vVhwrLiqmBiy1QL0sbpcxy6RmUJNXawuRQIDAQAB") },
+  ]
+}
--- src/servers.tf
+++ src/servers.tf
@@ -1,6 +1,7 @@
+# == z01.de ==
 resource "hcloud_server" "z01_de" {
   name        = "z01.de"
-  image       = "debian-12"
+  image       = "debian-13"
   server_type = "cx23"
   location    = "nbg1"
   ssh_keys    = [hcloud_ssh_key.zezav_id_ed25519.name]
@@ -35,15 +36,115 @@ resource "hcloud_zone_rrset" "zezav_cz_z01_de_aaaa" {
   ]
 }
 
-resource "hcloud_rdns" "master" {
+resource "hcloud_rdns" "z01_de" {
   server_id  = hcloud_server.z01_de.id
   ip_address = hcloud_server.z01_de.ipv4_address
   dns_ptr    = "z01.de.zezav.cz"
 }
 
-output "ips" {
+# == z02.de ==
+
+resource "hcloud_server" "z02_de" {
+  name        = "z02.de"
+  image       = "debian-13"
+  server_type = "cpx32"
+  location    = "nbg1"
+  ssh_keys    = [hcloud_ssh_key.zezav_id_ed25519.name]
+  labels = {
+    management    = "true"
+    management_by = "terraform"
+  }
+  firewall_ids = []
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+}
+
+resource "hcloud_zone_rrset" "zezav_cz_z02_de_a" {
+  zone = hcloud_zone.zezav_cz.name
+  name = hcloud_server.z02_de.name
+  type = "A"
+
+  records = [
+    { value = hcloud_server.z02_de.ipv4_address },
+  ]
+}
+
+resource "hcloud_zone_rrset" "zezav_cz_z02_de_aaaa" {
+  zone = hcloud_zone.zezav_cz.name
+  name = hcloud_server.z02_de.name
+  type = "AAAA"
+
+  records = [
+    { value = hcloud_server.z02_de.ipv6_address },
+  ]
+}
+
+resource "hcloud_rdns" "z02_de" {
+  server_id  = hcloud_server.z02_de.id
+  ip_address = hcloud_server.z02_de.ipv4_address
+  dns_ptr    = "z02.de.zezav.cz"
+}
+
+# == z03.de ==
+
+resource "hcloud_server" "z03_de" {
+  name        = "z03.de"
+  image       = "debian-13"
+  server_type = "cx23"
+  location    = "nbg1"
+  ssh_keys    = [hcloud_ssh_key.zezav_id_ed25519.name]
+  labels = {
+    management    = "true"
+    management_by = "terraform"
+  }
+  firewall_ids = []
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+}
+
+resource "hcloud_zone_rrset" "zezav_cz_z03_de_a" {
+  zone = hcloud_zone.zezav_cz.name
+  name = hcloud_server.z03_de.name
+  type = "A"
+
+  records = [
+    { value = hcloud_server.z03_de.ipv4_address },
+  ]
+}
+
+resource "hcloud_zone_rrset" "zezav_cz_z03_de_aaaa" {
+  zone = hcloud_zone.zezav_cz.name
+  name = hcloud_server.z03_de.name
+  type = "AAAA"
+
+  records = [
+    { value = hcloud_server.z03_de.ipv6_address },
+  ]
+}
+
+resource "hcloud_rdns" "z03_de" {
+  server_id  = hcloud_server.z03_de.id
+  ip_address = hcloud_server.z03_de.ipv4_address
+  dns_ptr    = "z03.de.zezav.cz"
+}
+
+output "server_ips" {
   value = {
-    ipv4 = hcloud_server.z01_de.ipv4_address
-    ipv6 = hcloud_server.z01_de.ipv6_address
+    "z01.de" = {
+      ipv4 = hcloud_server.z01_de.ipv4_address
+      ipv6 = hcloud_server.z01_de.ipv6_address
+    }
+    "z02.de" = {
+      ipv4 = hcloud_server.z02_de.ipv4_address
+      ipv6 = hcloud_server.z02_de.ipv6_address
+    }
+    "z03.de" = {
+      ipv4 = hcloud_server.z03_de.ipv4_address
+      ipv6 = hcloud_server.z03_de.ipv6_address
+    }
   }
 }
